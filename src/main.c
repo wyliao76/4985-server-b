128b89ef1d74c62d6d32de6b296e4e8553a5645d
Will Liao <wyliao76@gmail.com>
2025-02-04 22:14:57 -0800

    add signal

--- src/main.c
+++ src/main.c
@@ -4,21 +4,60 @@
 #include <errno.h>
 #include <memory.h>
 #include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
+#if defined(__linux__) && defined(__clang__)
+_Pragma("clang diagnostic ignored \"-Wdisabled-macro-expansion\"")
+#endif
+
 #define BUFLEN 1024
 #define INADDRESS "0.0.0.0"
 #define PORT "8081"
+#define SIG_BUF 50
+
+    static volatile sig_atomic_t running;    // NOLINT(cppcoreguidelines-avoid-non-const-global-variables,-warnings-as-errors)
+
+static const char *const ending = "\nShutting down gracefully...\n";
+
+static void handle_signal(int sig)
+{
+    char message[SIG_BUF];
+
+    snprintf(message, sizeof(message), "Caught signal: %d (%s)\n", sig, strsignal(sig));
+    write(STDOUT_FILENO, message, strlen(message));
+
+    if(sig == SIGINT)
+    {
+        running = 0;
+        snprintf(message, sizeof(message), "%s\n", ending);
+    }
+    write(STDOUT_FILENO, message, strlen(message));
+}
 
 int main(int argc, char *argv[])
 {
-    pid_t     pid;
-    int       sockfd;
-    Arguments args;
-    int       err;
+    struct sigaction sa;
+    pid_t            pid;
+    int              sockfd;
+    Arguments        args;
+    int              err;
+
+    sa.sa_handler = handle_signal;    // Set handler function for SIGINT
+    sigemptyset(&sa.sa_mask);         // Don't block any additional signals
+    sa.sa_flags = 0;
+
+    // Register signal handler
+    if(sigaction(SIGINT, &sa, NULL) == -1)
+    {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server launching... (press Ctrl+C to interrupt)\n");
 
     memset(&args, 0, sizeof(Arguments));
     args.addr = INADDRESS;
@@ -38,8 +77,9 @@ int main(int argc, char *argv[])
     }
 
     // Wait for client connections
-    pid = 1;
-    while(pid > 0)
+    err     = 0;
+    running = 1;
+    while(running)
     {
         int                connfd;
         struct sockaddr_in connaddr;
@@ -53,7 +93,7 @@ int main(int argc, char *argv[])
         connfd = accept(sockfd, (struct sockaddr *)&connaddr, &connsize);
         if(connfd < 0)
         {
-            perror("main::accept");
+            // perror("main::accept");
             continue;
         }
 
@@ -65,23 +105,26 @@ int main(int argc, char *argv[])
         if(pid < 0)
         {
             perror("main::fork");
+            close(connfd);
             continue;
         }
 
         if(pid == 0)
         {
-            close(sockfd);
-
             err = 0;
             if(copy(connfd, connfd, BUFLEN, &err) < 0)
             {
                 errno = err;
                 perror("main::copy");
             }
+            close(connfd);
         }
-
-        close(connfd);
+        else
+        {
+            close(connfd);
+        }
     }
+    close(sockfd);
 
     return EXIT_SUCCESS;
 }
